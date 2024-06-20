783ebf8152bc1558c570b3d075fd13e898c00e6f
Ilias_Paralikas <paralikasilias@gmail.com>
2024-06-20 10:45:07 +0300

    ok

--- decision_makers/dummies.py
+++ decision_makers/dummies.py
@@ -1,23 +1,20 @@
 from .decision_maker_base import DescisionMakerBase
 import numpy as np
 class AllLocal(DescisionMakerBase):
-    def __init__(self):
-        super().__init__()
 
-    def choose_action(self, state):
+    def choose_action(self):
         return 0
     
 class AllVertical(DescisionMakerBase):
-    def __init__(self, number_of_actions):
-        super().__init__()
+    def __init__(self, number_of_actions,*args, **kwargs):
         self.number_of_actions =number_of_actions
 
-    def choose_action(self, state):
+    def choose_action(self,*args, **kwargs):
         return self.number_of_actions - 1
 class AllHorizontal(DescisionMakerBase):
-    def __init__(self, number_of_actions):
+    def __init__(self, number_of_actions,*args, **kwargs):
         super().__init__()
         self.number_of_actions =number_of_actions
 
-    def choose_action(self, state):
+    def choose_action(self, *args, **kwargs):
         return np.random.randint(1,self.number_of_actions - 1)
